14878dbdc1b1fd2e772779d693c97c2baee08004
CaterinaBi <catherinebonan@gmail.com>
2022-07-14 16:42:52 +0100

    Creates file that implements bonus features

--- camera_rps_bonus_features.py
+++ camera_rps_bonus_features.py
@@ -4,65 +4,29 @@ import numpy as np
 import random
 import time
 '''
-    NEEDS UPDATING
-    A game of Rock, Paper, Scissors in which the user plays against the computer.
-    The user inputs their chosen gesture in writing.
-    The computer chooses its move randomly from a pre-determined list.
-
-    Parameters:
-    ----------
-    gesture_list: list
-        List of gestures to be used in the game
-
-    Attributes:
-    ----------
-    computer_choice: str
-        The gesture to be played by the computer, picked randomly from gesture_list
-    user_choice: str
-        The gesture played by the user (input)
-    winner: str
-        The winner of the match/game
-
-    Methods:
-    -------
-    get_computer_choice(computer_choice)
-        Gets the computer's input.
-    get_user_choice(user_choice)
-        Gets the user's input.
-    get_winner(computer_choice, user_choice, winner)
-        Returns the name of the winner.
-    '''
+An implementation of the programme in `camera_rps.pt that additionally prints messages on the screen.`    
+'''
 class Game:
   def __init__(self):
-    self.computer_choice = str
-
     # model, video and data attributes
     self.model = load_model('keras_model.h5')
     self.cap = cv2.VideoCapture(0)
     self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    # self.normalized_image = 0 # Normalize the image
-
-    # font to be used in text messages
-    # self.font = cv2.FONT_HERSHEY_SIMPLEX
-    # messages to be displayed
-    # self.intro_message = ""
-    # self.instruction_message = ""
-
-    self.user_choice = self.get_prediction()
+    # users attributes
     self.user = "user"
     self.computer = "computer"
-    self.winner = str
-    # self.seconds = 0
     self.round_number = 1
     self.computer_lives = 3
     self.user_lives = 3
-    # self.waitKey = 0
-
-    # layout miscellaneous prints
+    # layout print
     self.spacer = "\n --------------------------------------------------------"
+    # messages to be displayed on screen
+    self.font = cv2.FONT_HERSHEY_SIMPLEX # font to be used in text messages
+    self.intro_message = ""
+    self.instruction_message = ""
 
-  def get_computer_choice(self, computer_choice):
-    self.computer_choice = random.choice(gesture_list)
+  def get_computer_choice(self):
+    computer_choice = random.choice(gesture_list)
     return computer_choice
 
   def get_prediction(self):
@@ -72,43 +36,21 @@ class Game:
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
     self.data[0] = normalized_image
     prediction = self.model.predict(self.data)
-    # cv2.putText(frame, self.intro_message, (300, 600), self.font, 1,(255, 255, 255), 2, cv2.LINE_4)
-    # cv2.putText(frame, self.instruction_message, (300, 640), self.font, 1,(255, 255, 255), 2, cv2.LINE_4)
+    cv2.putText(frame, self.intro_message, (300, 600), self.font, 1,(255, 255, 255), 2, cv2.LINE_4)
+    cv2.putText(frame, self.instruction_message, (300, 640), self.font, 1,(255, 255, 255), 2, cv2.LINE_4)
     cv2.imshow('frame', frame)
     return prediction
-
-  def counter_1(self):
-    countdown = 3
-    print("\nPrepare to show me your chosen gesture in...")
-    while countdown > 0:
-      print(f'{countdown}')
-      cv2.waitKey(1000)
-      countdown -= 1
-    print('\nShow your hand NOW!')
-    self.counter_2()
-
-  def counter_2(self):
-    counter = 2
-    while counter > 0:
-      cv2.waitKey(1000)
-      print("...")
-      counter -= 1
   
-  # gets gesture out of prediction
   def classify_output(self):
-    """
-    Uses the list of probabilities output from get_prediction
-    to determine the image inputted in the camera.
-    """
     prediction = self.get_prediction()
     choice_probability = {'Rock': prediction[0,1], 'Paper': prediction[0,2], 'Scissors': prediction[0,3]}
     self.user_prediction = max(choice_probability, key=choice_probability.get)
     print(f"\nThe machine predicted that the user gesture was {self.user_prediction}.")
     return self.user_prediction
 
-  # determines winner
-  def get_winner(self, computer_choice, user_choice, winner):
-    computer_choice = self.get_computer_choice(computer_choice)
+  def get_winner(self):
+    winner = str
+    computer_choice = self.get_computer_choice()
     user_choice = self.classify_output()
     if computer_choice == user_choice:
       print(f"\nThe computer too chose {computer_choice}. No one wins this round!")
@@ -136,7 +78,7 @@ class Game:
     return winner
 
   def count_lives(self):
-    winner = self.get_winner(self.computer_choice, self.user_choice, self.winner)
+    winner = self.get_winner()
     self.counter_2()
     if winner == "user":
       self.computer_lives -= 1
@@ -154,18 +96,30 @@ class Game:
     if self.computer_lives == 0 or self.user_lives == 0:
       print(self.spacer, f"\n ******** GAME OVER! The {winner} wins the game! ********", self.spacer, "\n")
 
+  def counter_1(self):
+    countdown = 3
+    print("\nPrepare to show me your chosen gesture in...")
+    while countdown > 0:
+      print(f'{countdown}')
+      cv2.waitKey(1000)
+      countdown -= 1
+    print('\nShow your hand NOW!')
+    self.counter_2()
+
+  def counter_2(self):
+    counter = 2
+    while counter > 0:
+      cv2.waitKey(1000)
+      print("...")
+      counter -= 1
+
 def play_game():
-  # round_number = 1
   game = Game()
   while game.computer_lives >= 1 and game.user_lives >= 1:
-    # elif cv2.waitKey(10000) & 0xFF == ord('c'):
-    # game.intro_message = "WELCOME TO THE GAME OF ROCK, PAPER, SCISSORS!"
-    # game.instruction_message = "Press 'c' to continue, or 'q' to quit."
     print(game.spacer, f"\n ******************** ROUND NUMBER {game.round_number} ********************", game.spacer)
     print("\nPress 'c' to continue, or 'q' to quit.")
-    game.get_computer_choice(game.computer_choice)
+    game.get_computer_choice()
     game.counter_1()
-    # game.get_prediction() # already called within classify_output()
     game.count_lives()
     game.round_number += 1
     # Press q to close the window
