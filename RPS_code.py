fafbd3241cf33c69e288ce9c1edfdc550fd5627d
CaterinaBi <catherinebonan@gmail.com>
2022-07-19 21:46:52 +0100

    Adds timers

--- RPS_code.py
+++ RPS_code.py
@@ -59,6 +59,23 @@ class Game:
         # layout miscellaneous prints
         self.spacer = "\n --------------------------------------------------------"
 
+    def countdown_counter(self):
+        countdown = 3
+        print("\nPrepare to show me your chosen gesture in...")
+        while countdown > 0:
+            print(f'{countdown}')
+            cv2.waitKey(1000)
+            countdown -= 1
+        print('\nShow your hand NOW!')
+        # cv2.waitKey(1000)
+    
+    def counter_2(self):
+        counter = 2
+        while counter > 0:
+            cv2.waitKey(1000)
+            print("...")
+            counter -= 1
+    
     def get_computer_choice(self, computer_choice):
         # print(f"The computer choice is {computer_choice}")
         return computer_choice
@@ -90,6 +107,7 @@ class Game:
         prediction = self.get_prediction()
         choice_probability = {'Rock': prediction[0,1], 'Paper': prediction[0,2], 'Scissors': prediction[0,3]}
         self.user_prediction = max(choice_probability, key=choice_probability.get)
+        # self.counter_2()
         print(f"The machine predicted that the user gesture was {self.user_prediction}")
         return self.user_prediction
 
@@ -97,6 +115,7 @@ class Game:
     def get_winner(self, computer_choice, user_choice, winner):
         computer_choice = self.get_computer_choice(computer_choice)
         user_choice = self.classify_output()
+        self.counter_2()
         if computer_choice == user_choice:
             print(f"\nThe computer too chose {computer_choice}. No one wins this round!")
         elif computer_choice == "Rock":
@@ -122,16 +141,6 @@ class Game:
                 print(f"\nThe computer chose {computer_choice}. The user wins this round!")
         return winner
 
-    def countdown_counter(self):
-        countdown = 3
-        print("\nPrepare to show me your chosen gesture in...")
-        while countdown > 0:
-            print(f'{countdown}')
-            cv2.waitKey(1000)
-            countdown -= 1
-        print('\nShow your hand NOW!')
-        cv2.waitKey(1000)
-
     def remaining_lives(self):
         winner = self.get_winner(self.computer_choice, self.user_choice, self.winner)
         if winner == "user":
@@ -140,6 +149,7 @@ class Game:
         elif winner == "computer":
             self.user_lives -=1
             print(f"The user now has {self.user_lives} lives left.")
+        self.counter_2()
         if self.computer_lives == 0 or self.user_lives == 0:
             print(self.spacer, f"\n ****** GAME OVER! The {winner} wins the game! ******", self.spacer, "\n")
 
@@ -151,6 +161,7 @@ def play_game(gesture_list):
     game.get_camera()
     print(game.spacer, f"\n ************** ROUND NUMBER {round_number} **************", game.spacer)
     game.countdown_counter()
+    game.counter_2()
     # game.get_prediction()
     # game.classify_output()
     # user_prediction
